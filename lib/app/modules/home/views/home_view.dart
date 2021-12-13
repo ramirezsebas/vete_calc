c17c263d782197a8fe24cfb0905f90a6be5efac8
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2021-12-13 12:04:34 -0300

    Agregamos la navegacion entre habilitacion del final y nota final

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,14 +9,30 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('Vete Calc'),
         centerTitle: true,
+        backgroundColor: Colors.red,
       ),
       body: Center(
         child: Text(
           'HomeView is working',
           style: TextStyle(fontSize: 20),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Habilitación Final',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Nota Final',
+          ),
+        ],
+        currentIndex: controller.selectedIndex.value,
+        selectedItemColor: Colors.amber[800],
+        onTap: controller.selectIndex,
       ),
     );
   }
